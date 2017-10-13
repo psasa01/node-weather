9eba00386a03b90e5a9c8d1e6a1062c140999157
psasa01 <psasa01@gmail.com>
2017-10-13 14:24:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address)
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+}, (error, response, body) => {
+
+    if (error) {
+        console.log('Unable to connect to Google Srevers');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    }
+});
