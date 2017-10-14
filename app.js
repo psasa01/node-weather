61da20e576313166dc401c624480fa7980773cbd
psasa01 <psasa01@gmail.com>
2017-10-14 13:02:54 +0200

    141017

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yargs = require('yargs');
+/* const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
 
@@ -20,5 +20,23 @@ geocode.geocodeAddress(argv.address, (err, result) => {
         console.log(err);
     } else {
         console.log(JSON.stringify(result, undefined, 2));
+    }
+});
+
+
+*/
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/012b1089f6bca5ab119323674c411da1/43.8261875,18.3509568',
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Unable to connect to server!');
+    } else if (response.statusCode === 400) {
+        console.log('Unable to fetch weather');
+    } else if (response.statusCode === 200) {
+        console.log(body.currently.temperature);
     }
 });
